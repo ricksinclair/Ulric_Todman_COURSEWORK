c5ef5f7c2a608be56f3d32764671fc2607e04912
Ulric Todman <ricksinclair.nyc@gmail.com>
2019-07-22 15:33:23 -0400

    1st day classwork/homework

---
+++ 20190723/U1M1L1Katas/src/main/java/com/company/FormattedOutput.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class FormattedOutput {
+
+    public static void main(String[] args) {
+
+
+        String myName = "Rick";
+        int myAge = 31;
+
+
+        System.out.printf("My name is %s and I am %d years old.", myName, myAge);
+    }
+}
+ 
