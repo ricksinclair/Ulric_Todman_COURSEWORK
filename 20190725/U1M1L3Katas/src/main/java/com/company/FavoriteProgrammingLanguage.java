d6a0b3dd5eb11049902481b485c7876a013584f6
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-07-24 21:44:31 -0400

    Added additional comments

--- 20190725/U1M1L3Katas/src/main/java/com/company/FavoriteProgrammingLanguage.java
+++ 20190725/U1M1L3Katas/src/main/java/com/company/FavoriteProgrammingLanguage.java
@@ -1,20 +1,21 @@
 package com.company;
+//import Scanner class
 import java.util.Scanner;
 
 public class FavoriteProgrammingLanguage {
 
     public static void main(String[] args) {
-
+        //create new Scanner instance
         Scanner scanner = new Scanner(System.in);
-
+        //Ask user for favorite language
         System.out.println("What is your favorite programming language?");
         String userAnswer = scanner.nextLine();
-
+        //Keep asking until the magic word Java floats off their fingers.
         while(!userAnswer.equals("Java")){
             System.out.println("What is your favorite programming language?");
             userAnswer = scanner.nextLine();
         }
-
+        //Positive reinforcement of the subject
         System.out.println("That's what I was looking for! Java is definitely the answer!");
     }
 }
--- 20190725/U1M1L3Katas/src/main/java/com/company/YourAgeCan.java
+++ 20190725/U1M1L3Katas/src/main/java/com/company/YourAgeCan.java
@@ -1,17 +1,20 @@
 package com.company;
+
+//import Scanner class
 import java.util.Scanner;
 
 public class YourAgeCan {
 
     public static void main(String[] args) {
-
+        //Create scanner instance.
         Scanner scanner = new Scanner(System.in);
 
+        //Prompt for and store user's age.
         System.out.println("Please enter your age.");
         int userAge = Integer.parseInt(scanner.nextLine());
 
 
-        //store vars for ages so its all more readable.
+        //store vars for ages so its all more hunman-readable.
         int votingAge = 18;
         int drinkingAge = 21;
         int presidentialAge = 35;
@@ -21,23 +24,25 @@ public class YourAgeCan {
         int upperOcto = 89;
         int centenarian = 100;
 
+        //If they are old enough to vote
         if (userAge >= votingAge)
             System.out.println("Vote");
-
+        //If they are of drinking age or older
         if (userAge >= drinkingAge)
             System.out.println("Drink Alcohol");
-
+        //If they are of age to be president.
         if (userAge >= presidentialAge)
             System.out.println("Be president");
-
-
+        //if they can join AARP
         if (userAge >= aarpEligible)
             System.out.println("Is eligible for AARP");
-
+        //If they can retire .
         if (userAge >= retirementAge)
             System.out.println("Can retire");
+        //If they are an octogenarian.
         if (userAge >= lowerOcto && userAge <= upperOcto)
             System.out.println("is an octogenarian");
+        //If they are a centenarian
         if (userAge >= centenarian)
             System.out.println("more than a century old");
     }
