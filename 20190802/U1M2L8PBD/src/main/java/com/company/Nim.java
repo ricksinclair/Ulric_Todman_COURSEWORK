52a73c9fedba8a3c4ff980ff971ada3779c84c59
Ulric Todman <ricksinclair.nyc@gmail.com>
2019-08-04 01:36:13 -0400

    added more hw

--- 20190802/U1M2L8PBD/src/main/java/com/company/Nim.java
+++ 20190802/U1M2L8PBD/src/main/java/com/company/Nim.java
@@ -1,4 +1,24 @@
 package com.company;
-
+import java.util.Scanner;
 public class Nim {
+
+
+
+        public static int[] piles = new int[] {3,3,3};
+    public static boolean finished = false;
+    public static String userName1;
+    public static String userName2;
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Player 1, please enter your name: ");
+        userName1 = scanner.nextLine();
+        System.out.print("Player 2, please enter your name: ");
+        userName2 = scanner.nextLine();
+    }
+
+
+
 }
