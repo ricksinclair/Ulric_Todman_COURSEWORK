c8953552ea33aa8ad1e50f356ae107798f55cbc4
Ulric Todman <ricksinclair.nyc@gmail.com>
2019-08-07 00:53:03 -0400

    did more RPG from summative

--- U1-M2-Summative-Todman-Ulric/RPGInheritance/src/main/java/com/company/Character.java
+++ U1-M2-Summative-Todman-Ulric/RPGInheritance/src/main/java/com/company/Character.java
@@ -2,5 +2,39 @@ package com.company;
 
 public abstract class Character {
 
+    protected String name;
+    protected int strength;
+    protected int health;
+    protected int stamina;
+    protected int speed;
+    protected int attackPower;
+
+
+
+    public void run() {
+
+    }
+
+    public void attack() {
+
+    }
+
+    public void heal() {
+
+    }
+
+    public void decreaseHealth() {
+
+    }
+
+    public void increaseStamina() {
+
+    }
+
+    public void decreaseStamina() {
+
+
+    }
+
 
 }
---
+++ U1-M2-Summative-Todman-Ulric/RPGInheritance/src/main/java/com/company/Farmer.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Farmer extends Character {
+
+    public Farmer(String name) {
+        this.name = name;
+        this.strength = 75;
+        this.health = 100;
+        this.stamina = 75;
+        this.speed = 10;
+        this.attackPower = 1;
+         
+
+    }
+
+
+    public void plow(){
+
+    }
+
+    public void harvest(){
+
+    }
+
+}
