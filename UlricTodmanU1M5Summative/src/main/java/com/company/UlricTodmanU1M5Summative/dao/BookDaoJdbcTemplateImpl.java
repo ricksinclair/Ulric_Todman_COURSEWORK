2fc499ae6e7879971089c3ce843c1825e616d322
Ulric Todman <ricksinclair.nyc@gmail.com>
2019-08-26 20:06:09 -0400

    completed impls and tests. ALL TESTS GREEN. Pending yaml and API endpoints

--- UlricTodmanU1M5Summative/src/main/java/com/company/UlricTodmanU1M5Summative/dao/BookDaoJdbcTemplateImpl.java
+++ UlricTodmanU1M5Summative/src/main/java/com/company/UlricTodmanU1M5Summative/dao/BookDaoJdbcTemplateImpl.java
@@ -1,37 +1,105 @@
 package com.company.UlricTodmanU1M5Summative.dao;
 
 import com.company.UlricTodmanU1M5Summative.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class BookDaoJdbcTemplateImpl implements BookDao {
+    //Prepared Statements.
+    private static final String INSERT_BOOK_SQL =
+            "INSERT INTO book (isbn, publish_date, author_id, publisher_id, title, price) VALUES (?, ?, ?, ?, ?, ?)";
+    private static final String SELECT_BOOK_SQL =
+            "SELECT * FROM book WHERE book_id=?";
+    private static final String SELECT_ALL_BOOKS_SQL =
+            "SELECT * FROM book";
+    private static final String SELECT_BOOKS_BY_AUTHOR_SQL =
+            "SELECT * FROM book where author_id = ?";
+    private static final String UPDATE_BOOK_SQL =
+            "UPDATE book SET isbn = ?, publish_date = ?, author_id = ?, publisher_id = ?, title = ?, price = ? WHERE book_id = ?";
+    private static final String DELETE_BOOK_SQL =
+            "DELETE FROM book WHERE book_id = ?";
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDaoJdbcTemplateImpl(JdbcTemplate newJdbcTemplate){
+        this.jdbcTemplate = newJdbcTemplate;
+    }
+
+    //Object mapper helper method
+    private Book mapRowToBook(ResultSet rs, int rowNum) throws SQLException{
+        Book book = new Book();
+
+        book.setTitle(rs.getString("title"));
+        book.setBookId(rs.getInt("book_id"));
+        book.setPublishDate(rs.getDate("publish_date"));
+        book.setAuthorId(rs.getInt("author_id"));
+        book.setPrice(rs.getBigDecimal("price"));
+        book.setPublisherId(rs.getInt("publisher_id"));
+        book.setIsbn(rs.getString("isbn"));
+
+        return book;
+    }
+
     @Override
+    @Transactional
     public Book addBook(Book book) {
-        return null;
+        // "INSERT INTO book (isbn, publish_date, author_id, publisher_id, title, price) VALUES (?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(INSERT_BOOK_SQL,
+                book.getIsbn(),
+                book.getPublishDate(),
+                book.getAuthorId(),
+                book.getPublisherId(),
+                book.getTitle(),
+                book.getPrice());
+
+        int id = jdbcTemplate.queryForObject("SELECT last_insert_id()", Integer.class);
+        book.setBookId(id);
+        return book;
     }
 
     @Override
     public Book getBook(int bookId) {
-        return null;
+        try{
+            return jdbcTemplate.queryForObject(SELECT_BOOK_SQL, this::mapRowToBook, bookId);
+        }catch(EmptyResultDataAccessException e){
+            return null;
+        }
     }
 
     @Override
     public List<Book> getAllBooks() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_BOOKS_SQL, this::mapRowToBook);
     }
 
     @Override
     public List<Book> getBooksByAuthor(int authorId) {
-        return null;
+        return jdbcTemplate.query(SELECT_BOOKS_BY_AUTHOR_SQL, this::mapRowToBook, authorId);
     }
 
     @Override
     public void updateBook(Book book) {
-
+        jdbcTemplate.update(UPDATE_BOOK_SQL,
+                book.getIsbn(),
+                book.getPublishDate(),
+                book.getAuthorId(),
+                book.getPublisherId(),
+                book.getTitle(),
+                book.getPrice(),
+                book.getBookId()
+                );
     }
 
     @Override
     public void deleteBook(int bookId) {
-
+        jdbcTemplate.update(DELETE_BOOK_SQL, bookId);
     }
 }
--- UlricTodmanU1M5Summative/src/test/java/com/company/UlricTodmanU1M5Summative/dao/BookDaoJdbcTemplateImplTest.java
+++ UlricTodmanU1M5Summative/src/test/java/com/company/UlricTodmanU1M5Summative/dao/BookDaoJdbcTemplateImplTest.java
@@ -15,6 +15,9 @@ import java.math.BigDecimal;
 import java.sql.Date;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class BookDaoJdbcTemplateImplTest {
@@ -28,12 +31,13 @@ public class BookDaoJdbcTemplateImplTest {
     @Before
     public void setUp() throws Exception{
         //clean out the test db
+        List<Book> bookList = bookDao.getAllBooks();
+        bookList.stream().forEach(book -> bookDao.deleteBook(book.getBookId()));
+
         List<Author> authorList = authorDao.getAllAuthors();
         authorList.stream().forEach(author -> authorDao.deleteAuthor(author.getAuthorId()));
         List<Publisher> publisherList = publisherDao.getAllPublishers();
         publisherList.stream().forEach(publisher -> publisherDao.deletePublisher(publisher.getPublisherId()));
-        List<Book> bookList = bookDao.getAllBooks();
-        bookList.stream().forEach(book -> bookDao.deleteBook(book.getBookId()));
 
 
     }
@@ -76,6 +80,11 @@ public class BookDaoJdbcTemplateImplTest {
         book1.setTitle("Java: A love story");
         book1 = bookDao.addBook(book1);
 
+        Book book2 = bookDao.getBook(book1.getBookId());
+        assertEquals(book1, book2);
+        bookDao.deleteBook(book1.getBookId());
+        book2 = bookDao.getBook(book1.getBookId());
+        assertNull(book2);
     }
 
     @Test
@@ -116,7 +125,111 @@ public class BookDaoJdbcTemplateImplTest {
         book2.setPublishDate(Date.valueOf("2019-08-26"));
         book2.setTitle("Java vs Javascript: Tales of a rivalry misunderstood");
         book2 = bookDao.addBook(book2);
+        List<Book> bookList = bookDao.getAllBooks();
+        assertEquals(bookList.size(), 2);
 
+    }
+
+    @Test
+    public void getBooksByAuthor(){
+        //add publishers and authors as prerequisite table entries/foreign keys.
+        Publisher publisher = new Publisher();
+        publisher.setName("Random House");
+        publisher.setStreet("123 Sesame Place");
+        publisher.setCity("New York");
+        publisher.setState("NY");
+        publisher.setPostalCode("10013");
+        publisher.setEmail("randomEmail@randomhouse.com");
+        publisher.setPhone("212-867-5309");
+        publisher = publisherDao.addPublisher(publisher);
+
+        Author author = new Author();
+        author.setFirstName("John");
+        author.setLastName("Doe");
+        author.setStreet("123 Main Street");
+        author.setCity("New York");
+        author.setState("NY");
+        author.setPostalCode("10013");
+        author.setPhone("212-555-2332");
+        author.setEmail("john.doe@gmail.com");
+        author = authorDao.addAuthor(author);
+        Author author2 = new Author();
+        author2.setFirstName("Johnny");
+        author2.setLastName("Dope");
+        author2.setStreet("456 Main Street");
+        author2.setCity("New York");
+        author2.setState("NY");
+        author2.setPostalCode("10026");
+        author2.setPhone("212-555-2332");
+        author2.setEmail("johnny@hotmail.com");
+        author2 = authorDao.addAuthor(author2);
+        Book book1 = new Book();
+        book1.setIsbn("82973918273");
+        book1.setAuthorId(author.getAuthorId());
+        book1.setPublisherId(publisher.getPublisherId());
+        book1.setPrice(BigDecimal.valueOf(17.99));
+        book1.setPublishDate(Date.valueOf("2019-08-26"));
+        book1.setTitle("Java: A love story");
+        book1 = bookDao.addBook(book1);
+        Book book2 = new Book();
+        book2.setIsbn("82973918673");
+        book2.setAuthorId(author.getAuthorId());
+        book2.setPublisherId(publisher.getPublisherId());
+        book2.setPrice(BigDecimal.valueOf(17.99));
+        book2.setPublishDate(Date.valueOf("2019-08-26"));
+        book2.setTitle("Java vs Javascript: Tales of a rivalry misunderstood");
+        book2 = bookDao.addBook(book2);
+
+        Book book3 = new Book();
+        book3.setIsbn("82973918673");
+        book3.setAuthorId(author2.getAuthorId());
+        book3.setPublisherId(publisher.getPublisherId());
+        book3.setPrice(BigDecimal.valueOf(17.99));
+        book3.setPublishDate(Date.valueOf("2019-08-26"));
+        book3.setTitle("Java vs Javascript: Tales of a rivalry misunderstood");
+        book3 = bookDao.addBook(book3);
+        List<Book> bookList = bookDao.getBooksByAuthor(author.getAuthorId());
+        assertEquals(bookList.size(), 2);
+
+        List<Book> fullBookList = bookDao.getAllBooks();
+        assertEquals(fullBookList.size(), 3);
+
+    }
+    @Test
+    public void updateBook(){
+        //add publishers and authors as prerequisite table entries/foreign keys.
+        Publisher publisher = new Publisher();
+        publisher.setName("Random House");
+        publisher.setStreet("123 Sesame Place");
+        publisher.setCity("New York");
+        publisher.setState("NY");
+        publisher.setPostalCode("10013");
+        publisher.setEmail("randomEmail@randomhouse.com");
+        publisher.setPhone("212-867-5309");
+        publisher = publisherDao.addPublisher(publisher);
+        Author author = new Author();
+        author.setFirstName("John");
+        author.setLastName("Doe");
+        author.setStreet("123 Main Street");
+        author.setCity("New York");
+        author.setState("NY");
+        author.setPostalCode("10013");
+        author.setPhone("212-555-2332");
+        author.setEmail("john.doe@gmail.com");
+        author = authorDao.addAuthor(author);
+        Book book1 = new Book();
+        book1.setIsbn("82973918273");
+        book1.setAuthorId(author.getAuthorId());
+        book1.setPublisherId(publisher.getPublisherId());
+        book1.setPrice(BigDecimal.valueOf(17.99));
+        book1.setPublishDate(Date.valueOf("2019-08-26"));
+        book1.setTitle("Java: A love story");
+        book1 = bookDao.addBook(book1);
+
+        book1.setTitle("A WORK IN PROGRESS: MY LIFE CODING JAVA");
+        bookDao.updateBook(book1);
+        Book book2 = bookDao.getBook(book1.getBookId());
+        assertEquals(book1, book2);
     }
 
 
