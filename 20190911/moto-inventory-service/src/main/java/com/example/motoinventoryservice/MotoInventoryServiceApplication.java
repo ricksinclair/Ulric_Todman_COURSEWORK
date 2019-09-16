3c2bcc93678613a23246dd39f9658342a50355e4
Ulric Todman <ricksinclair.nyc@gmail.com>
2019-09-16 08:14:11 -0400

    added hw

--- 20190911/moto-inventory-service/src/main/java/com/example/motoinventoryservice/MotoInventoryServiceApplication.java
+++ 20190911/moto-inventory-service/src/main/java/com/example/motoinventoryservice/MotoInventoryServiceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class MotoInventoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MotoInventoryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MotoInventoryServiceApplication.class, args);
+    }
 
 }
