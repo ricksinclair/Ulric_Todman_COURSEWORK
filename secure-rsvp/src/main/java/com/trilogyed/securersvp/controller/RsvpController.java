224408fb3cf2ee9bc331744d67a9d301b41d7930
Ulric Todman <ricksinclair.nyc@gmail.com>
2019-09-24 12:58:35 -0400

    finished last of service layer tests

--- secure-rsvp/src/main/java/com/trilogyed/securersvp/controller/RsvpController.java
+++ secure-rsvp/src/main/java/com/trilogyed/securersvp/controller/RsvpController.java
@@ -1,4 +1,19 @@
 package com.trilogyed.securersvp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RsvpController {
+
+    @RequestMapping(value= "/publicEvent", method = RequestMethod.GET)
+    public String viewPublicEvents(){
+        return "Here are the public events for your joy.";
+    }
+
+    @RequestMapping(value = "/privateEvent", method = RequestMethod.GET)
+    public String viewPrivateEvents(){
+        return "Here are the PRIVATE events for your consideration.";
+    }
 }
--- secure-rsvp/src/main/java/com/trilogyed/securersvp/util/PasswordUtility.java
+++ secure-rsvp/src/main/java/com/trilogyed/securersvp/util/PasswordUtility.java
@@ -1,4 +1,14 @@
 package com.trilogyed.securersvp.util;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class PasswordUtility {
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        String password = "password";
+        String encodedPassword = passwordEncoder.encode(password);
+        System.out.println(encodedPassword);
+    }
 }
